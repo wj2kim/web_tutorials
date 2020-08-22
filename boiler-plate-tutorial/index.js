4ecf8c60f1ad13d4cb7709314eadb4f92b85b718
paulwoojungkim <wj2kim@gmail.com>
2020-08-22 17:24:59 +0900

    connected mongooseDB

--- boiler-plate-tutorial/index.js
+++ boiler-plate-tutorial/index.js
@@ -1,2 +1,21 @@
 // this js file is entry point for the backend
 
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb+srv://paul_01:123123123@cluster0.dx66t.mongodb.net/<dbname>?retryWrites=true&w=majority',
+ {useNewUrlParser: true })
+    .then(() => console.log('DB connected'))
+    .catch(err => console.error(err));
+
+
+// using http get method
+app.get('/', (req, res) => {
+    res.send("Hello World!");
+});
+
+
+// port number 5000 is standard port number for node.js
+app.listen(5000);
